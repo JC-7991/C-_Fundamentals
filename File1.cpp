36ea8e4dbbdffdb017fa8f9ae69953185461d941
JC <jctblock@gmail.com>
2023-02-14 14:44:19 -0500

    Checkpoint

--- File1.cpp
+++ File1.cpp
@@ -1,1 +1,5 @@
-# 
+/*Things to brush up on:
+C++: abstraction, inheritance, constructors, classes, structs, keywords, method overloading vs method overriding
+Multi-threading, deadlocks
+Bitwise manipulations
+*/
