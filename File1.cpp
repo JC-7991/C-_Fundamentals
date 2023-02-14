43a75e6e540d49d3a2aa394e380827cb78c448ca
JC <jctblock@gmail.com>
2023-02-14 15:28:20 -0500

    Checkpoint

--- File1.cpp
+++ File1.cpp
@@ -3,3 +3,25 @@ C++: abstraction, inheritance, constructors, classes, structs, keywords, method 
 Multi-threading, deadlocks
 Bitwise manipulations
 */
+using namespace std;
+#include <iostream>
+
+class Test{
+
+   public:
+
+        Test(){
+            std::cout<<"\n Constructor executed";
+        }   
+ 
+        ~Test(){
+            std::cout<<"\n Destructor executed";
+        }
+};
+
+main(){
+    
+    Test t;   
+    return 0;
+    
+}
