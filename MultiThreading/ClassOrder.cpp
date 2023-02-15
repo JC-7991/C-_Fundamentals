f84921705e7c5ec463f0324635864f634277ce98
JC <jctblock@gmail.com>
2023-02-15 10:46:46 -0500

    Checkpoint

--- MultiThreading/ClassOrder.cpp
+++ MultiThreading/ClassOrder.cpp
@@ -3,17 +3,18 @@
 class A{
 
     public:
-    A(){
-        std::cout << "A\n";
-    }
+        A(){
+            std::cout << "A\n";
+        }
 
 };
 
 class B: public A{
+
     public:
-    B(){
-        std::cout << "B\n";
-    }
+        B(){
+            std::cout << "B\n";
+        }
 
 };
 
