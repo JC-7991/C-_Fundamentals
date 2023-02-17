6848c2f25189679e3f4b633ff0cd4fd1d956137d
JC <jctblock@gmail.com>
2023-02-17 00:00:33 -0500

    Checkpoint

--- MultiThreading/FunctionOrder2.cpp
+++ MultiThreading/FunctionOrder2.cpp
@@ -1,12 +1,10 @@
-#include <iostream> // for std::cout
+#include <iostream>
 
-void doB()
-{
+void doB(){
     std::cout << "In doB()\n";
 }
 
-void doA()
-{
+void doA(){
     std::cout << "In doA()\n";
 
     doB();
