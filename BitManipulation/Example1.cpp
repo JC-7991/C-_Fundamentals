822dccbc3c89196f479dbf5771db6b172109ea5a
JC <jctblock@gmail.com>
2023-02-19 10:25:08 -0500

    Checkpoint

--- BitManipulation/Example1.cpp
+++ BitManipulation/Example1.cpp
@@ -3,11 +3,11 @@
 
 int main(){
 
-    std::bitset<8> bits{ 0b0000'0101 }; // we need 8 bits, start with bit pattern 0000 0101
+    std::bitset<8> bits{ 0b0000'0101 }; // We need 8 bits, start with bit pattern 0000 0101.
 
-    bits.set(3); // set bit position 3 to 1 (now we have 0000 1101)
-    bits.flip(4); // flip bit 4 (now we have 0001 1101)
-    bits.reset(4); // set bit 4 back to 0 (now we have 0000 1101)
+    bits.set(3); // Set bit position 3 to 1 (now we have 0000 1101).
+    bits.flip(4); // Flip bit 4 (now we have 0001 1101).
+    bits.reset(4); // Set bit 4 back to 0 (now we have 0000 1101).
 
     std::cout << "All the bits: " << bits << '\n';
     std::cout << "Bit 3 has value: " << bits.test(3) << '\n';
