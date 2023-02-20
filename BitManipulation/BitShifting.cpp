f960950673d04f5ecdb5cd7a8f44adb879f5a886
JC <jctblock@gmail.com>
2023-02-20 00:08:32 -0500

    Checkpoint

--- BitManipulation/BitShifting.cpp
+++ BitManipulation/BitShifting.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 
 int main(){
-    std::bitset<4> x { 0b1100 };
 
+    std::bitset<4> x { 0b1100 };
     std::cout << x << '\n';
     std::cout << (x >> 1) << '\n'; // Shift right by 1, yielding 0110.
     std::cout << (x << 1) << '\n'; // Shift left by 1, yielding 1000.
 
     return 0;
+
 }
