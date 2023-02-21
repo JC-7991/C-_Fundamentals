27ada296ed7ae5288e69f947ab77359808ef11d7
JC <jctblock@gmail.com>
2023-02-21 14:31:27 -0500

    Checkpoint

--- Classes/Example1.cpp
+++ Classes/Example1.cpp
@@ -24,11 +24,11 @@ int main(){
    Box2.breadth = 13.0;
 
    volume = Box1.height * Box1.length * Box1.breadth;
-   cout << "Volume of Box1 : " << volume <<endl;
+   cout << "Volume of Box1 : " << volume << "\n";
 
    volume = Box2.height * Box2.length * Box2.breadth;
-   cout << "Volume of Box2 : " << volume <<endl;
+   cout << "Volume of Box2 : " << volume << "\n";
 
    return 0;
-   
+
 }
