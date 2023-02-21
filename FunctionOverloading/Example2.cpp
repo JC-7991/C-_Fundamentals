97af17891ee9e9d0554f4838c483ee03d359ef37
JC <jctblock@gmail.com>
2023-02-21 15:07:42 -0500

    Checkpoint

--- FunctionOverloading/Example2.cpp
+++ FunctionOverloading/Example2.cpp
@@ -13,26 +13,31 @@ class Box{
             length = len;
         }
 
-      void setBreadth( double bre ) {
-         breadth = bre;
-      }
-      void setHeight( double hei ) {
-         height = hei;
-      }
+        void setBreadth(double bre){
+            breadth = bre;
+        }
+
+        void setHeight(double hei){
+            height = hei;
+        }
+
+        Box operator+(const Box& b){
+
+            Box box;
+
+            box.length = this->length + b.length;
+            box.breadth = this->breadth + b.breadth;
+            box.height = this->height + b.height;
+
+            return box;
+
+        }
       
-      // Overload + operator to add two Box objects.
-      Box operator+(const Box& b) {
-         Box box;
-         box.length = this->length + b.length;
-         box.breadth = this->breadth + b.breadth;
-         box.height = this->height + b.height;
-         return box;
-      }
-      
-   private:
-      double length;      // Length of a box
-      double breadth;     // Breadth of a box
-      double height;      // Height of a box
+    private:
+        double length;
+        double breadth;
+        double height;
+        
 };
 
 // Main function for the program
