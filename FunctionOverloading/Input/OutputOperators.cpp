b8e08647354434cb65d3d2bf4a84c7e8344d9832
JC <jctblock@gmail.com>
2023-02-23 00:02:43 -0500

    Checkpoint

--- FunctionOverloading/Input/OutputOperators.cpp
+++ FunctionOverloading/Input/OutputOperators.cpp
@@ -35,13 +35,13 @@ int main(){
 
    Distance D1(11, 10), D2(5, 11), D3;
 
-   cout << "Enter the value of object : " << endl;
+   cout << "Enter the value of object: " << endl;
    cin >> D3;
 
-   cout << "First Distance : " << D1 << endl;
-   cout << "Second Distance :" << D2 << endl;
-   cout << "Third Distance :" << D3 << endl;
+   cout << "First Distance: " << D1 << endl;
+   cout << "Second Distance: " << D2 << endl;
+   cout << "Third Distance: " << D3 << endl;
 
    return 0;
-   
+
 }
