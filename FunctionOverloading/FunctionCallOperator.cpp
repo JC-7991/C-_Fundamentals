9e27a4257595e9ad900556523136be32f1acd442
JC <jctblock@gmail.com>
2023-02-27 00:08:55 -0500

    Checkpoint

--- FunctionOverloading/FunctionCallOperator.cpp
+++ FunctionOverloading/FunctionCallOperator.cpp
@@ -28,10 +28,10 @@ class Distance{
 
         }
       
-        void displayDistance() {
+        void displayDistance(){
             cout << "F: " << feet << " I:" << inches << endl;
         }
-        
+
 };
 
 int main() {
