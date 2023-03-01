abd57b0d2606798a66682895c843fb6b7724a921
JC <jctblock@gmail.com>
2023-03-01 12:15:14 -0500

    Checkpoint

--- Polymorphism/Example1.cpp
+++ Polymorphism/Example1.cpp
@@ -52,16 +52,10 @@ int main(){
    Rectangle rec(10, 7);
    Triangle  tri(10, 5);
 
-   // store the address of Rectangle
    shape = &rec;
-
-   // call rectangle area.
    shape->area();
 
-   // store the address of Triangle
    shape = &tri;
-
-   // call triangle area.
    shape->area();
 
    return 0;
