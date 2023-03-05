a67691f996dabd738f255e55312432bcac3d3598
JC <jctblock@gmail.com>
2023-03-04 23:59:48 -0500

    Checkpoint

--- Interfaces/Example1.cpp
+++ Interfaces/Example1.cpp
@@ -2,10 +2,11 @@
 using namespace std;
  
 class Shape{
-    
+
    public:
-      // pure virtual function providing interface framework.
-      virtual int getArea() = 0;
+
+    virtual int getArea() = 0;
+    
       void setWidth(int w) {
          width = w;
       }
